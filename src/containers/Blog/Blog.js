f5b1422ea7f4db403223bae3418058ab17aab2e1
ciiay <redrainyday@gmail.com>
2021-01-12 23:49:22 -0500

    Added fetching and display data from axios calls

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: [],
+        selectedPostId: null,
+    };
+    componentDidMount() {
+        axios
+            .get('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                // console.log(response);
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map((post) => {
+                    return {
+                        ...post,
+                        author: 'Max',
+                    };
+                });
+                this.setState({ posts: updatedPosts });
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    };
+
+    render() {
+        const posts = this.state.posts.map((post) => {
+            return (
+                <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />
+            );
+        });
         return (
             <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
+                <section className='Posts'>{posts}</section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
